5450a0afce9ada608a4e00a083e41798dc5dbfd7
Chris Hill <yorkshiretable@gmail.com>
2013-11-11 14:19:37 +0000

    tidied up the code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 7
 # first, we print the list of students
 puts "The students og my cohort at Makers Academy"
 puts"-----------------"
@@ -9,7 +10,4 @@ puts "Ken"
 puts "Asta"
 puts "G"
 # finally, we print the total
-print "Overall, we have "
-# it's important that print() doesn't add new lines of characters
-print 7
-puts " great students"
+puts "Overall, we have #{student_count} great students"
