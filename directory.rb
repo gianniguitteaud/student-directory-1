9491cda4187d3be4c59d0c05fed9127f0957ea20
Chris Hill <yorkshiretable@gmail.com>
2013-11-11 14:44:32 +0000

    added ruby methods

--- directory.rb
+++ directory.rb
@@ -8,11 +8,22 @@ students = [
 	"Asta"
 	"G"
 ]
-# and then print them
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-students.each do |student|
-	puts student
+def print_header
+	puts "the students of my cohort at makers academy"
+	puts "--------------"
 end
-# finally, we print the total
-puts "Overall, we have #{students.length} great students"
+
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+	puts "overall we have #{names.length} great students"
+end
+
+#nothing happens until we call methods
+print_header
+print (students)
+print_footer (students)
