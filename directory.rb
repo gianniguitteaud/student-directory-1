c637d6f5d222eab3f4e28ddd5ae4f0a6bb3571ab
chrishill627 <yorkshiretable@gmail.com>
2013-11-14 13:52:15 +0000

    added ability to input data

--- directory.rb
+++ directory.rb
@@ -7,24 +7,39 @@ students = [
 	{:name => "Ken", :cohort =>  :november},
 	{:name => "Nicki", :cohort =>  :november},
 	{:name => "Asta", :cohort =>  :november},
-	{:name => "G", :cohort =>  :november}
+	{:name => "G", :cohort =>  :november},
+  {:name => "Tom", :cohort =>  :november},
+  {:name => "Kumi", :cohort =>  :november}
 ]
 def print_header
 	puts "the students of my cohort at makers academy"
 	puts "--------------"
 end
 
-def print(students)
+def print(students) 
 	students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
-def print_footer(names)
+def print_footer(names)	
 	puts "overall we have #{names.length} great students"
 end
 
+def input_students
+  puts "please enter the names of the students"
+  puts "to finish hit return twice"
+students=[]
+name =gets.chomp.split(' ').map {|word| word.capitalize }.join(' ')
+while !name.empty? do
+  students << {:name => name, :cohort => :november}
+  puts "now we have #{students.length} students"
+  name = gets.chomp.split(' ').map {|word| word.capitalize }.join(' ')
+end
+  students
+end
 #nothing happens until we call methods
+students = input_students
 print_header
 print (students)
 print_footer (students)
